c35eaae1bbf4e48472921677154f54765ed1b178
matheus <matheusnbbg@hotmail.com>
2022-11-08 18:05:15 -0300

    Adicionado funcionalidade de ao clicar na senha gerada ela sera salva no clipboard

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // console.log('Hello World!');
 
 import { nanoid } from 'nanoid'
+import copy  from "clipboard-copy"
 import './style.css'
 
 const passwordBtnEl = document.querySelector('button');
@@ -9,4 +10,9 @@ const displayPasswordEl = document.querySelector('h2');
 passwordBtnEl.addEventListener('click', () => {
     const randomPassword = nanoid()
     displayPasswordEl.innerHTML = randomPassword;
+})
+
+displayPasswordEl.addEventListener('click', () => {
+    copy(displayPasswordEl.innerHTML)
+    alert('Senha copiada')
 })
